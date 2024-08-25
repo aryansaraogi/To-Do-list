c266f7049bdf6874c77426b16a8628e18482bd2b
Aryan Sarogi <aryansaraogi02@gmail.com>
2024-08-25 14:27:27 +0530

    time date and csss

--- app.js
+++ app.js
@@ -11,35 +11,37 @@ function addTaskToHTML(){
         let newTask = document.createElement('li');
         newTask.classList.add(task.status);
         newTask.innerHTML = `
-        <div class="complete-icon" oncLick="completeTask(${index})">
+        <div class="complete-icon" onclick="completeTask(${index})">
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10 2 2 4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
             </svg>
         </div>
         <div class="content">
+            <div class="date-time">${task.createdAt}</div> <!-- Date and time positioned here -->
             ${task.content}
         </div>
-        <div class="close-icon" oncLick="deleteTask(${index})">
+        <div class="close-icon" onclick="deleteTask(${index})">
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
             </svg>
         </div>
         `;
         list.appendChild(newTask);
-
-        
     })
 }
+
 function completeTask(index){
     listTask[index].status = 'complete';
     addTaskToHTML();
     saveLocalStorage();
 }
+
 function deleteTask(index){
-    listTask =  listTask.filter((task, key) =>{return  key != index} );
+    listTask =  listTask.filter((task, key) => { return key != index });
     addTaskToHTML();
     saveLocalStorage();
 }
+
 addTaskToHTML();
 
 btnAdd.onclick = function(event){
@@ -48,13 +50,15 @@ btnAdd.onclick = function(event){
     if(content != ''){
         listTask.unshift({
             content: content,
-            status: 'doing'
-        })
+            status: 'doing',
+            createdAt: new Date().toLocaleString() // Store the current date and time
+        });
     }
     document.getElementById('task').value = '';
     addTaskToHTML();
     saveLocalStorage();
 }
+
 function saveLocalStorage(){
     localStorage.setItem('listTask', JSON.stringify(listTask));
 }
